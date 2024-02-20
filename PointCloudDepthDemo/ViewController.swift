6db400e8ce7923c98f1b24a9f6233c906e7eba46
tuyw <18759280805@163.com>
2024-02-21 00:36:53 +0800

    feat: 基本代码

--- PointCloudDepthDemo/ViewController.swift
+++ PointCloudDepthDemo/ViewController.swift
@@ -11,7 +11,7 @@ import ARKit
 class ViewController: UIViewController {
     
     var sceneView: ARSCNView!
-    
+    var pointCloudView: PointCloud!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +19,12 @@ class ViewController: UIViewController {
         
         sceneView = ARSCNView(frame: view.bounds)
         sceneView.session.delegate = self
+        sceneView.debugOptions = .showWorldOrigin
         view.addSubview(sceneView)
+        
+        pointCloudView = PointCloud()
+        sceneView.scene.rootNode.addChildNode(pointCloudView)
+
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -43,9 +48,39 @@ class ViewController: UIViewController {
 extension ViewController: ARSessionDelegate {
     func session(_ session: ARSession, didUpdate frame: ARFrame) {
         guard let depthMap = frame.sceneDepth?.depthMap else { return }
+        let capturedImage = frame.capturedImage
         
         let w = CVPixelBufferGetWidth(depthMap)
         let h = CVPixelBufferGetHeight(depthMap)
+        let cameraIntrinsics = frame.camera.intrinsics
+        
+        let bytesPerRow = CVPixelBufferGetBytesPerRow(depthMap) / MemoryLayout<Float>.size
+        
+        CVPixelBufferLockBaseAddress(depthMap, CVPixelBufferLockFlags(rawValue: 0))
+        let floatBuffer = unsafeBitCast(CVPixelBufferGetBaseAddress(depthMap), to: UnsafeMutablePointer<Float>.self)
+        
+        var pointCloud = [simd_float3]()
+        
+        for y in 0..<h {
+            for x in 0..<w {
+                let pixel = floatBuffer[y * bytesPerRow + x]
+                if pixel != 0 {
+                    let depth = Float(pixel)
+                    let xrw = (Float(x) - cameraIntrinsics[2][0]) * depth / cameraIntrinsics[0][0]
+                    let yrw = (Float(y) - cameraIntrinsics[2][1]) * depth / cameraIntrinsics[1][1]
+
+                    let point = simd_float3(x: xrw, y: yrw, z: depth)
+                    
+                    pointCloud.append(point)
+                }
+            }
+        }
+        
+        CVPixelBufferUnlockBaseAddress(depthMap, CVPixelBufferLockFlags(rawValue: 0))
+        
+        let point = simd_float3(x: 0.1, y: 0.1, z: 0.1)
+        pointCloudView.updatePoints([point])
+        
     }
     
 }
